65366d4b6b8fcff9054f531d6d9b24ac554c6ade
smartestbadboy <134474362+smartestbadboy@users.noreply.github.com>
2023-06-21 13:54:43 +0600

    Add files via upload

--- bubblesort.java
+++ bubblesort.java
@@ -1,1 +1,31 @@
-System.out.println("bubblesort");
+public class bubblesort{
+	static void bubblesort(int [] arr){
+		int n=arr.length;
+		int temp=0;
+		for(int i=0; i<n-i; i++){
+			for(int j=1; j<(n-i); j++){
+				if(arr[j-1]>arr[j]){
+				temp = arr[j-1];
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
+public static void main(String [] args){
+	int arr[]={3,60,35,2,45,320,5};
+	System.out.println("array before bubblesort");
+	for(int i=0; i<arr.length; i++){
+			System.out.println(arr[i]+" ");
+	}
+		System.out.println();
+		bubblesort(arr);
+		System.out.println("array after bubblesort");
+		for(int i=0; i<arr.length; i++){
+			System.out.println(arr[i]+" ");
+		}
+	}
+}
+
+
+		
